642441af9ca77447f0902bd3b0200a678b0963df
augustine-awuori <codewithaugustine@gmail.com>
2024-11-03 17:40:23 +0300

    Go back from 'view invoices pages'

--- src/main/java/com/example/health_system/ViewInvoicesController.java
+++ src/main/java/com/example/health_system/ViewInvoicesController.java
@@ -1,11 +1,17 @@
 package com.example.health_system;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
@@ -21,6 +27,8 @@ public class ViewInvoicesController {
     private TableColumn<Invoice, String> paymentModeColumn;
     @FXML
     private TableColumn<Invoice, LocalDate> dateColumn;
+    @FXML
+    private Button backButton;
 
     @FXML
     public void initialize() {
@@ -60,7 +68,15 @@ public class ViewInvoicesController {
 
     @FXML
     private void handleBack() {
-        // Logic to close this window or go back to the previous scene.
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/health_system/main_layout.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) backButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void showAlert(Alert.AlertType alertType, String title, String content) {
